f892981d210bc7320f5ec8bdc4871f580f2fe748
wayne james <jackmcwilliams@Jacks-Air.lan>
2022-10-31 06:45:15 -0700

    schema constraints added

--- schema.sql
+++ schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE department (
 CREATE TABLE job (
   id INT NOT NULL,
   title VARCHAR(30),
-  salary VARCHAR(30),
+  salary INT NOT NULL,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
@@ -22,7 +22,7 @@ CREATE TABLE employee (
   id INT NOT NULL,
   PRIMARY KEY (id),
   first_name VARCHAR(30) NOT NULL,
-  last_name INT,
+  last_name VARCHAR(30) NOT NULL,
   FOREIGN KEY (job_id)
   REFERENCES job(id)
   ON DELETE SET NULL
