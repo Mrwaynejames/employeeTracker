6320f1c99c0165a305a4a052dad5149f18a342d5
wayne james <jackmcwilliams@Jacks-Air.lan>
2022-10-31 07:20:49 -0700

    2 tables info complete

---
+++ seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO department(id, dep_name)
+VALUES  (001, "General"),
+        (002, "Ortho"),
+        (003, "Neuro"),
+        (004, "Peds"),
+        (005, "Trauma"),
+        (006, "Cardio"),
+        (007, "Intern");
+
+INSERT INTO job(id,title,salary,department_id)
+        ()
