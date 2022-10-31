f4f2c15bfeaac963a464ff4ddf9c7c984563b51a
wayne james <jackmcwilliams@Jacks-Air.lan>
2022-10-31 07:39:36 -0700

    employee table in progress

--- seed.sql
+++ seed.sql
@@ -1,11 +1,22 @@
 INSERT INTO department(id, dep_name)
-VALUES  (001, "General"),
-        (002, "Ortho"),
-        (003, "Neuro"),
-        (004, "Peds"),
-        (005, "Trauma"),
-        (006, "Cardio"),
-        (007, "Intern");
+VALUES  (010, "General"),
+        (020, "Ortho"),
+        (030, "Neuro"),
+        (040, "Peds"),
+        (050, "Trauma"),
+        (060, "Cardio"),
+        (070, "Intern");
 
 INSERT INTO job(id,title,salary,department_id)
+VALUES  (100, "Surgical Intern", 60000, "Inten")
+        (200, "Resident", 110000, "Ortho")
+        (300, "Chief Resident", 120000, "Trauma")
+        (400, "Attending", 250000, "General" )
+        (500, "Department Head", 300000, "Neuro")
+        (600, "Chief of surgery", 600000, "General");
+
+INSERT INTO employee(id,first_name, last_name, job_id, manager_id)
+VALUES  (171, "Meredith", "Grey", 100, )
+        (221, "Caliope", "Torres", 200, )
+        (531, "Derrek", "Sheperd", 500, )
         ()
