7a3e2834c720277bd49c5d5c76bf0a516b1d3e13
wayne james <jackmcwilliams@Jacks-Air.lan>
2022-10-31 20:52:34 -0700

    schema and seed are functioning

--- schema.sql
+++ schema.sql
@@ -3,29 +3,31 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
+
 CREATE TABLE department (
-  id INT NOT NULL,
-  dep_name VARCHAR(30),
-  PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dep_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE job (
-  id INT NOT NULL,
+  id INT NOT NULL PRIMARY KEY,
   title VARCHAR(30),
   salary INT NOT NULL,
+  department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
-  PRIMARY KEY (id),
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
+  job_id INT,
+  manager_id INT,
   FOREIGN KEY (job_id)
   REFERENCES job(id)
-  ON DELETE SET NULL
+  ON DELETE SET NULL,
   FOREIGN KEY (manager_id)
   REFERENCES employee(id)
   ON DELETE SET NULL
--- seed.sql
+++ seed.sql
@@ -1,22 +1,26 @@
-INSERT INTO department(id, dep_name)
-VALUES  (010, "General"),
-        (020, "Ortho"),
-        (030, "Neuro"),
-        (040, "Peds"),
-        (050, "Trauma"),
-        (060, "Cardio"),
-        (070, "Intern");
+INSERT INTO department(dep_name)
+VALUES  ("General"),
+        ("Ortho"),
+        ("Neuro"),
+        ("Peds"),
+        ("Trauma"),
+        ("Cardio"),
+        ("Intern");
 
 INSERT INTO job(id,title,salary,department_id)
-VALUES  (100, "Surgical Intern", 60000, "Inten")
-        (200, "Resident", 110000, "Ortho")
-        (300, "Chief Resident", 120000, "Trauma")
-        (400, "Attending", 250000, "General" )
-        (500, "Department Head", 300000, "Neuro")
-        (600, "Chief of surgery", 600000, "General");
+VALUES  (100,"Surgical Intern",60000,3),
+        (200,"Resident",110000,2),
+        (300,"Chief Resident",120000,5),
+        (400,"Attending",250000,1),
+        (500,"Department Head",300000,3),
+        (600,"Chief of Surgery",600000,1);
 
-INSERT INTO employee(id,first_name, last_name, job_id, manager_id)
-VALUES  (171, "Meredith", "Grey", 100, )
-        (221, "Caliope", "Torres", 200, )
-        (531, "Derrek", "Sheperd", 500, )
-        ()
+INSERT INTO employee(first_name,last_name,job_id,manager_id)
+VALUES  ("Meredith","Grey",100,3),
+        ("Caliope","Torres",200,4),
+        ("Derek","Shepherd",500,4),
+        ("Richard","Webber",600,0),
+        ("Teddy","Altman",400,4),
+        ("April","Kepner",300,5),
+        ("Cristina","Yang",100,5),
+        ("Mark","Sloan",500,4);
